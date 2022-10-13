14e4d7ec4220c8584e6776ab832ab5739d581f65
salus-sage <salus.sage@gmail.com>
2022-10-13 20:21:34 +0530

    tic tac toe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react';
+import "./App.css";
+
+function Board() {
+  const [cells, setCells] = React.useState(Array(9).fill(null));
+
+  const nextValue = calculateTurn(cells);
+  const winner = calculateWinner(cells);
+  const status = isGameOver(winner, cells, nextValue);
+
+  function selectCell(cell) {
+    //check if won or all cells full
+    if (winner || cells[cell]) {
+      return;
+    }
+    const cellsCopy = [...cells];
+    cellsCopy[cell] = nextValue;
+    setCells(cellsCopy);
+  }
+
+  function restart() {
+    setCells(Array(9).fill(null));
+  }
+
+  function renderCell(i) {
+    return (
+      <button className="cell" onClick={() => selectCell(i)}>
+        {cells[i]}
+      </button>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="game-info">{status}</div>
+      <div className="board-row">
+        {renderCell(0)}
+        {renderCell(1)}
+        {renderCell(2)}
+      </div>
+      <div className="board-row">
+        {renderCell(3)}
+        {renderCell(4)}
+        {renderCell(5)}
+      </div>
+      <div className="board-row">
+        {renderCell(6)}
+        {renderCell(7)}
+        {renderCell(8)}
+      </div>
+      <button className="restart" onClick={restart}>
+        restart
+      </button>
     </div>
   );
 }
 
-export default App;
+function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+    </div>
+  );
+}
+
+// Check if game over
+function isGameOver(winner, cells, nextValue) {
+  //ToDo: this could be refactored
+  if(winner) {
+    return `Winner: ${winner}`;
+  }
+  if(cells.every(Boolean)) {
+    return `Oops!: Its a tie`;
+  }
+  return `Next player: ${nextValue}`;
+  
+}
+
+//check player turn
+function calculateTurn(cells) {
+  return cells.filter(Boolean).length % 2 === 0 ? 'X' : 'O';
+}
+
+/* 
+calculte winner,
+the winning patterns are saved in an array, with the index
+of top left cell starting at 0 and bottom right endng at 8.
+ex. row 0 = [0, 1, 2] or column 0 = [0, 3, 6]
+*/
+function calculateWinner(cells) {
+  const winningPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < winningPatterns.length; i++) {
+    const [a, b, c] = winningPatterns[i];
+    if (cells[a] && cells[a] === cells[b] && cells[a] === cells[c]) {
+      return cells[a];
+    }
+  }
+  return null;
+}
+
+function App() {
+  return <Game />
+}
+
+export default App
